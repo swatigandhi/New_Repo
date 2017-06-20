d52a2d3c45f31984f153d93208c9d9ccd5a9d51b
swatigandhi <swatigandhi@qainfotech.com>
2017-06-20 11:28:21 +0530

    changes in bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,10 @@
 <?php
 // This is global bootstrap for autoloading
+     echo "deleting file";
+    echo "\n";
+    unlink("vendor".DIRECTORY_SEPARATOR."codeception".DIRECTORY_SEPARATOR."codeception".DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."Codeception".DIRECTORY_SEPARATOR."Module".DIRECTORY_SEPARATOR."WebDriver.php");
+    echo "Copying file";
+  echo "\n";    
+    copy("PHP_Files".DIRECTORY_SEPARATOR."WebDriver.php", "vendor".DIRECTORY_SEPARATOR."codeception".DIRECTORY_SEPARATOR."codeception".DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."Codeception".DIRECTORY_SEPARATOR."Module".DIRECTORY_SEPARATOR."WebDriver.php");
+   
+    
